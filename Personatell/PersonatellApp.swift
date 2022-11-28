aca7cc6dffcb262f205ac1619220ba7142c94164
Can <hcdayan@hotmail.com>
2022-11-28 08:08:49 +0100

    Initial Commit

---
+++ Personatell/PersonatellApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonatellApp.swift
+//  Personatell
+//
+//  Created by Can on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonatellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
