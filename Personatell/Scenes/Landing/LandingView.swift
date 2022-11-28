ec1edc76537a624328c674407af6252546fe1063
Can <hcdayan@hotmail.com>
2022-11-28 20:13:23 +0100

    Refactor landing view.

--- Personatell/Scenes/Landing/LandingView.swift
+++ Personatell/Scenes/Landing/LandingView.swift
@@ -11,22 +11,31 @@ struct LandingView: View {
     @State var shouldShowTest: Bool = false
     private var viewModel = LandingViewModel()
     
+    private let iconWidth = 150.0
+    
     var body: some View {
         if shouldShowTest {
             QuestionsView()
         } else {
             VStack(spacing: 12.0) {
+                Spacer()
                 Image(systemName: "person.fill.questionmark")
-                    .imageScale(.large)
+                    .resizable()
+                    .imageScale(.small)
+                    .frame(width: iconWidth, height: iconWidth)
                     .foregroundColor(.purple)
                 Text(viewModel.title)
                     .font(.headline)
                 Text(viewModel.subtitle)
                     .font(.subheadline)
+                Spacer()
                 Button {
                     shouldShowTest = true
                 } label: {
-                    Text(viewModel.buttonTitle)
+                    HStack {
+                        Image(systemName: "play")
+                        Text(verbatim: viewModel.buttonTitle)
+                    }
                 }
             }
             .padding()
